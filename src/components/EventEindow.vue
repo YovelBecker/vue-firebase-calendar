58ae2e14500a52fa603837fe8daf33c7094b604a
Yovel Becker <yovelb13@gmail.com>
2019-11-22 04:18:02 +0200

    first commit

---
+++ src/components/EventEindow.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-menu bottom right>
+    <template v-slot:activator="{ on }">
+      <v-btn outlined v-on="on">
+        <span>{{ typeToLabel[type] }}</span>
+        <v-icon right>mdi-menu-down</v-icon>
+      </v-btn>
+    </template>
+    <v-list>
+      <v-list-item @click="type = 'day'">
+        <v-list-item-title>Day</v-list-item-title>
+      </v-list-item>
+      <v-list-item @click="type = 'week'">
+        <v-list-item-title>Week</v-list-item-title>
+      </v-list-item>
+      <v-list-item @click="type = 'month'">
+        <v-list-item-title>Month</v-list-item-title>
+      </v-list-item>
+      <v-list-item @click="type = '4day'">
+        <v-list-item-title>4 days</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,31 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueTextareaAutosize from 'vue-textarea-autosize'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 import './registerServiceWorker'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+
+Vue.use(VueTextareaAutosize)
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAauj0-vqB5DufjM3Kpy3pcK3WSzhIVcfE",
+  authDomain: "vue-calendar-5b3e0.firebaseapp.com",
+  databaseURL: "https://vue-calendar-5b3e0.firebaseio.com",
+  projectId: "vue-calendar-5b3e0",
+  storageBucket: "vue-calendar-5b3e0.appspot.com",
+  messagingSenderId: "927733517940",
+  appId: "1:927733517940:web:41bf8feef9c58c76e08b21"
+})
+
+export const db = firebase.firestore();
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
